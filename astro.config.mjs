f0fc9f69e4db9f2bd08c76f190c78a6792d58c1c
Laura Lopez <lauralop_11@hotmail>
2025-03-03 17:54:21 -0500

    prueba2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/portfolio-laura/", // Reemplaza con el nombre de tu repositorio
+  output: "static", // Asegura que Astro genere archivos estáticos
   vite: {
     plugins: [tailwindcss()],
   },
